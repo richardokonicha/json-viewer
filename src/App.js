0791c9f22f632e6ec71c113477af77b968b7134b
Richard Okonicha <richardokonicha@gmail.com>
2021-01-27 17:58:24 +0000

    import json and view data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({data}) {
+  console.log(data)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{data.name}</p>
+        <p>{data.description}</p>
+        <p>{data.polygon}</p>
+        <p>{data.details}</p>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import example from './example.json'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App data={example} />
   </React.StrictMode>,
   document.getElementById('root')
 );
